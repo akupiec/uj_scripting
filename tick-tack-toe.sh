cde0780f52b475c5b6ba8366a68495bd925d2491
akupiec <kupiecart@gmail.com>
2024-11-15 00:04:15 +0100

    diagonal & antidiagonal

--- tick-tack-toe.sh
+++ tick-tack-toe.sh
@@ -3,7 +3,7 @@ source setup_board.sh
 
 boardArray=""
 
-BOARD_SIZE=3
+BOARD_SIZE=4
 #WINING_LENGTH=3
 EMPTY_CHAR='_'
 SAVE_FILE='autosave.txt'
@@ -38,7 +38,8 @@ hex_to_num() {
 }
 
 draw_board() {
-  rows=(${boardArray//./ })
+  to_print=${1-$boardArray}
+  rows=(${to_print//./ })
 
   for ((i = 0; i < $BOARD_SIZE; i++)); do
     (($i == 0)) && echo -en "\t"
@@ -58,12 +59,52 @@ generate_reverted_board() {
     for ((j = 0; j < $BOARD_SIZE; j++)); do
       idx=$(((i + (j * $BOARD_SIZE)) * 2))
       reverted+=${boardArray:idx:1}
-      [[ $j < $((BOARD_SIZE - 1)) ]] && reverted+="|"
     done
     [[ $i < $((BOARD_SIZE - 1)) ]] && reverted+="."
   done
 
   echo $reverted
+}
+
+generate_diagonal_board() {
+  reverted=""
+  rows=(${boardArray//./ })
+  for ((i = 0; i < $BOARD_SIZE; i++)); do
+    x=$i
+    r0=""
+    r1=""
+    for ((j = 0; j < $BOARD_SIZE; j++)); do
+      r0=$r0$j$x" "
+      (($j!=$x)) && r1=$r1$x$j" "
+      ((x++))
+      if (($x == $BOARD_SIZE)); then
+        break
+      fi
+    done
+    echo $r0
+    echo $r1
+  done
+}
+
+generate_anti_diagonal_board() {
+  reverted=""
+  rows=(${boardArray//./ })
+  len=$((BOARD_SIZE-1))
+  for ((i = $len; i >= 0; i--)); do
+    x=$i
+    r0=""
+    r1=""
+    for ((j = 0; j < $BOARD_SIZE; j++)); do
+      r0=$r0$j$x" "
+      (($i != $len)) && r1=$r1$(($len - x))$(($len - j))" "
+      ((x--))
+      if (($x < 0)); then
+        break
+      fi
+    done
+    echo $r0
+    echo $r1
+  done
 }
 
 assert() {
@@ -132,11 +173,19 @@ check_win() {
 read_board
 save_board
 clear_console
-#draw_board
+
+draw_board
+echo
+#draw_board "$(generate_reverted_board)"
+#draw_board "$(generate_diagonal_board)"
+
+generate_reverted_board
+generate_diagonal_board
+generate_anti_diagonal_board
 #input=$(takePlayerInput "X")
 #make_move "X" "$input"
-check_win
-end=$(check_win)$?
-echo $end
+#check_win
+#end=$(check_win)$?
+#echo $end
 
 #draw_board

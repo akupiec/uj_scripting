9de694e145da79f9b6e0a55a78c3204a5e9efba5
akupiec <kupiecart@gmail.com>
2024-11-15 00:04:15 +0100

    js: it's actually building :D

--- js/minecraft_schema_builder/src/generate.ts
+++ js/minecraft_schema_builder/src/generate.ts
@@ -7,12 +7,14 @@ const out = await parseSchema("./simple-castle.schematic");
 
 const base1023RAW = fs.readFileSync("./src/schemaTools/decodeBase1023.ts", "utf-8");
 const lz77RAW = fs.readFileSync("./src/schemaTools/lz77Decompress.ts", "utf-8");
+const idxToCordsRaw = fs.readFileSync("./src/indexToCords.ts", "utf-8");
 
 
 let mc_script = "";
 mc_script += "let data :any = " + JSON.stringify(out) + ";\n";
 mc_script += base1023RAW.replaceAll("export ", "");
 mc_script += lz77RAW.replaceAll("export ", "");
+mc_script += idxToCordsRaw.replaceAll("export ", "");
 mc_script += `
 
 const blockData: any[] = lz77Decompress(decodeBase1023(data.data));
@@ -21,20 +23,15 @@ const width = size[1];
 const height = size[0];
 const llength = size[2];
 
-function indexToCoords(index: number, dimX: number, dimY: number, dimZ: number) {
-  const x = Math.floor(index / (dimY * dimZ));
-  const y = Math.floor((index % (dimY * dimZ)) / dimZ);
-  const z = index % dimZ;
-  return pos( x, y, z + 2);
-}
-
 const playerpos = pos(0,0,0).toWorld();
-blocks.fill(AIR, playerpos, pos(width + 10, height + 10, llength + 10), FillOperation.Replace);
+blocks.fill(AIR, pos(-4, 0, -4) , pos(width + 10, height + 10, llength + 10), FillOperation.Replace);
 
-for (let i = 0; i < 500; i++) {
+for (let i = 0; i < blockData.length; i++) {
     if (blockData[i] == 0) continue;
-    const p = playerpos.add(indexToCoords(i, width, height, llength));
-    player.say("place block " + blockData[i] + " " + p)
+    const {x,y,z} = indexToCords(i, height, width, llength)
+    const p = playerpos.add(pos(z, x, y + 5))
+    // player.say("place block " + blockData[i] + " " + p)
+    player.say("place block " + i + " of " + length)
     blocks.place(blockData[i], p);
 }
 `
